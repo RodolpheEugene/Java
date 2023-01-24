8b72133279644fe008bd01ea8519e8939cb7931e
Rodolphe S. Eugene <92120435+RodolpheEugene@users.noreply.github.com>
2023-01-24 03:06:56 -0500

    Add files via upload

---
+++ Assignment 2/GeometricObject.java
@@ -0,0 +1,39 @@
+public class GeometricObject {
+
+    // declare variables
+    private String color = "White";
+    private boolean filled;
+    private java.util.Date dateCreated;
+
+    // Constructors
+    public GeometricObject(){
+        dateCreated = new java.util.Date();
+    }
+    public GeometricObject(String color, boolean filled){
+        dateCreated = new java.util.Date();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    // Accessors and Mutators
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String Color){
+        this.color = color;
+    }
+    public boolean isFilled(){
+        return filled;
+    }
+    public void setFilled(boolean fiiled){
+        this.filled = filled;
+    }
+    public java.util.Date getDateCreated(){
+        return dateCreated;
+    }
+
+    // Methods
+    public String toString(){
+        return "created on " + dateCreated + "\ncolor: " + color + " and filled: " + filled;
+    }
+}
